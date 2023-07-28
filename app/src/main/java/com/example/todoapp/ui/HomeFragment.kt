7faf7820b5be37e7547b3166996a7ac503d1478d
ecembostancioglu <ecembostancioglu21@gmail.com>
2023-07-28 15:47:29 +0300

    Created fragment home

--- app/src/main/java/com/example/todoapp/ui/HomeFragment.kt
+++ app/src/main/java/com/example/todoapp/ui/HomeFragment.kt
@@ -1,9 +1,14 @@
 package com.example.todoapp.ui
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.todoapp.R
+import com.example.todoapp.data.data.model.Note
+import com.example.todoapp.data.data.source.NoteDatabase
 import com.example.todoapp.databinding.FragmentHomeBinding
 import com.example.todoapp.viewBinding
 
@@ -20,7 +25,81 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         super.onViewCreated(view, savedInstanceState)
 
         with(binding) {
+            setData(NoteDatabase.getNotes())
+
+            fabAdd.setOnClickListener {
+                showAddDialog()
+            }
         }
     }
+    private fun setData(list: List<Note>) {
+        binding.rv_home.adapter = dailyNotesAdapter
+        dailyNotesAdapter.updateList(list)
+    }
+
+    private fun onNoteClick(desc: String) {
+        Toast.makeText(requireContext(), desc, Toast.LENGTH_SHORT).show()
+    }
+
+
+    private fun showAddDialog() {
+        val builder = AlertDialog.Builder(requireContext())
+        val binding = AddAlertDialogDesignBinding.inflate(layoutInflater)
+        builder.setView(binding.root)
+        val dialog = builder.create()
+
+        val saveTypeList = listOf("Daily", "Bookmark")
+
+        var selectedSaveType = ""
+
+        var selectedDate = ""
+
+        val saveTypeAdapter = ArrayAdapter(
+            requireContext(), androidx.transition.R.layout.support_simple_spinner_dropdown_item, saveTypeList
+        )
+
+          with(binding) {
+
+            val calendar = Calendar.getInstance()
+
+            actSaveType.setAdapter(saveTypeAdapter)
+
+            actSaveType.setOnItemClickListener { _, _, position, _ ->
+                selectedSaveType = saveTypeList[position]
+            }
+
+            switchDate.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) {
+                    showDatePicker(calendar) { year, month, day ->
+                        showTimePicker(calendar) { hour, minute ->
+                            selectedDate = "$day.$month.$year - $hour:$minute"
+                            tvDate.text = "$day.$month.$year - $hour:$minute"
+                            tvDate.visibility = View.VISIBLE
+                        }
+                    }
+                }
+            }
+
+            btnAdd.setOnClickListener {
+                val title = etTitle.text.toString()
+                val desc = etDesc.text.toString()
+
+                if (title.isNotEmpty() && desc.isNotEmpty() && selectedSaveType.isNotEmpty()) {
+                    Database.addDailyNote(title, desc, selectedDate, selectedSaveType)
+                    setData(Database.getDailyNotes())
+                    dialog.dismiss()
+                }
+            }
+
+            btnCancel.setOnClickListener {
+                dialog.dismiss()
+            }
+        }
+
+        dialog.show()
+    }
+
+
+
 
 }
